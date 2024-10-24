8f388b2e8cdc13e7550775c90fbdb699f09f529e
Ezequiel <ezequielgaribottovillanueva@gmail.com>
2024-10-24 17:44:36 +0200

    start

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+radio.onReceivedString(function (undefined) {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("HOLA")
+    basic.showString("ENVIADO")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    numero_aleatorio = randint(1, 6)
+    radio.sendValue("numA", numero_aleatorio)
+    basic.showNumber(numero_aleatorio)
+})
+let numero_aleatorio = 0
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
